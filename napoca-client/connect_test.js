c055b2c8d87259ba965cec2125179ea1bb193ac8
Darius Marian <marian.darius98@gmail.com>
2019-04-22 15:27:09 +0300

    Don't test the received string in Napoca's connection test, only that the string was received, and thus the connection is working.

--- napoca-client/connect_test.js
+++ napoca-client/connect_test.js
@@ -7,13 +7,8 @@ const client = new napoca_client.NapocaClient("localhost:" + port);
 
 client.requestParse("Hello, World!", (tikz) => {
     console.log("[Napoca Client] Received response: '" + tikz + "'");
-    if (tikz === "Hello, World!") {
-        console.log("[Napoca Client] Success!");
-        process.exit(0);
-    } else {
-        console.log("[Napoca Client] Response is unexpected!");
-        process.exit(1);
-    }
+    console.log("[Napoca Client] Success!");
+    process.exit(0);
 }, (err) => {
     console.error("[Napoca Client] Failed with error: ", err);
     process.exit(1);
