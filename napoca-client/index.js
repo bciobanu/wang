0ecac36a47811a300bd0a3b0f3fe8dcee1a5f89c
Darius Marian <marian.darius98@gmail.com>
2019-04-22 16:50:49 +0300

    Better error message definition

--- napoca-client/index.js
+++ napoca-client/index.js
@@ -15,16 +15,37 @@ exports.NapocaClient = class NapocaClient {
             defaults: true,
             oneofs: true
         });
-        this._protoDescriptor = grpc.loadPackageDefinition(this._packageDefinition);
-        this._grpcClient = new this._protoDescriptor.wang.Napoca(serverSpec, grpc.credentials.createInsecure());
+        this._proto = grpc.loadPackageDefinition(this._packageDefinition).wang;
+        this._grpcClient = new this._proto.Napoca(serverSpec, grpc.credentials.createInsecure());
     }
 
+    /**
+     * @param wangCode The request to be handled.
+     * @param successCallback A function to be called with the tikz code as argument, if the request was successful.
+     * @param errorCallback A function to be called with an array of errors as argument, if the request failed.
+     * Errors are in the format described in the napoca.proto file (they have a code and a description).
+     * An example of an error is the following:
+     *  {
+     *      code: {
+     *          name: 'NETWORK_ERROR',
+     *          number: 1,
+     *          options: null
+     *      },
+     *      description: 'Error: 14 UNAVAILABLE: Connect Failed'
+     *  }
+     *  The format is the same for all errors (network & compiler).
+     */
     requestParse(wangCode, successCallback, errorCallback) {
         this._grpcClient.parse({
             wang_code: wangCode
         }, (err, wangResponse) => {
             if (err != null) {
-                errorCallback(err);
+                errorCallback([{
+                    code: this._proto.Error.type.enumType[0].value[1],
+                    description: err.toString()
+                }]);
+            } else if (wangResponse.errors.length > 0) {
+                errorCallback(wangResponse.errors);
             } else {
                 successCallback(wangResponse.tikz);
             }
