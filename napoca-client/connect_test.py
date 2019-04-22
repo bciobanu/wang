e7d6d6faa88377a4773a18939adce86596809388
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2019-04-22 22:26:35 +0300

    Make napoca-client tests work on Windows

--- napoca-client/connect_test.py
+++ napoca-client/connect_test.py
@@ -4,11 +4,11 @@ import subprocess
 from time import sleep
 
 # Start a napoca server on port 50001.
-napoca_server = subprocess.Popen(["../target/debug/napoca", "50001"])
+napoca_server = subprocess.Popen(["../target/debug/napoca", "3000"])
 
 sleep(1)
 
-napoca_client_ret_code = subprocess.call(["node", "./connect_test.js", "50001"])
+napoca_client_ret_code = subprocess.call(["node", "./connect_test.js", "3000"])
 
 napoca_server.kill()
 
