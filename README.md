b91203de3f47618980241c5aacee67403292a418
Darius Marian <marian.darius98@gmail.com>
2019-04-22 13:09:16 +0300

    Add dummy Napoca implementation using protocol buffers

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wang
 [![Build Status](https://travis-ci.org/bciobanu/wang.svg?branch=master)](https://travis-ci.org/bciobanu/wang)
+
+# Build
+To build the Napoca server locally, a `protoc` executable is required,
+at least at version `3.0`.
+- On Mac OS X: `brew install protobuf` / `brew upgrade protobuf`.
+If this still doesn't work, try:
+```bash
+PROTOC_ZIP=protoc-3.7.1-osx-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+rm -f $PROTOC_ZIP
+```
+- On Linux:
+```bash
+PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+rm -f $PROTOC_ZIP
+```
+Then, just running `cargo build` will generate the required Protocol Buffer files.
--- napoca/src/main.rs
+++ napoca/src/main.rs
@@ -1,3 +1,50 @@
+extern crate protobuf;
+extern crate grpc;
+extern crate futures;
+extern crate futures_cpupool;
+extern crate tls_api;
+extern crate tls_api_stub;
+extern crate tls_api_native_tls;
+
+pub mod napoca;
+pub mod napoca_grpc;
+
+use std::thread;
+
+use grpc::ServerBuilder;
+use grpc::SingleResponse;
+use grpc::RequestOptions;
+use tls_api_stub::TlsAcceptor;
+
+use napoca::WangRequest;
+use napoca::WangResponse;
+use napoca_grpc::Napoca;
+use napoca_grpc::NapocaServer;
+
+struct NapocaImpl;
+
+impl Napoca for NapocaImpl {
+    fn parse(&self, _: RequestOptions, mut req: WangRequest) -> SingleResponse<WangResponse> {
+        let mut r = WangResponse::new();
+        println!("Wang request with code '{}'", req.get_wang_code());
+        r.set_tikz(req.take_wang_code());
+        return SingleResponse::completed(r);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = 50001;
+
+    let mut server: ServerBuilder<TlsAcceptor> = grpc::ServerBuilder::new();
+    server.http.set_port(port);
+    server.http.set_cpu_pool_threads(4);
+    server.add_service(NapocaServer::new_service_def(NapocaImpl));
+
+    let _server = server.build().expect("server");
+
+    println!("Napoca server started on port {}", port);
+
+    loop {
+        thread::park();
+    }
 }
