95d3b9dc3cad3f0039b08a8ef79f43e92976febc
Darius Marian <marian.darius98@gmail.com>
2019-04-24 22:26:30 +0300

    Use material list instead of simple list

--- panda/lib/components/wang_page/wang_page.dart
+++ panda/lib/components/wang_page/wang_page.dart
@@ -1,7 +1,8 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_list/material_list.dart';
+import 'package:angular_components/material_list/material_list_item.dart';
 import 'package:angular_components/material_spinner/material_spinner.dart';
 import 'package:panda/components/figure_editor/figure_editor.dart';
-import 'package:panda/services/auth_service.dart';
 import 'package:panda/services/figures_service.dart';
 
 @Component(
@@ -10,21 +11,20 @@ import 'package:panda/services/figures_service.dart';
   styleUrls: ['wang_page.css'],
   directives: [
     MaterialSpinnerComponent,
+    MaterialListComponent,
+    MaterialListItemComponent,
     FigureEditorComponent,
     coreDirectives,
   ],
 )
 class WangPageComponent implements OnInit {
-  final AuthService _authService;
   final FiguresService _figuresService;
 
   bool loadedFigures = false;
   Iterable<Figure> figures;
   int highlightedFigureId = null;
 
-  WangPageComponent(this._authService, this._figuresService);
-
-  String get username => _authService.ownUsername;
+  WangPageComponent(this._figuresService);
 
   @override
   void ngOnInit() async {
