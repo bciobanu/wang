f94b7997b1b4ac1d4861c6d81895e5609b35821d
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2019-04-24 20:36:10 +0300

    Change port for napoca server in connection test

--- napoca-client/connect_test.py
+++ napoca-client/connect_test.py
@@ -4,11 +4,11 @@ import subprocess
 from time import sleep
 
 # Start a napoca server on port 50001.
-napoca_server = subprocess.Popen(["../target/debug/napoca", "3000"])
+napoca_server = subprocess.Popen(["../target/debug/napoca", "3001"])
 
 sleep(1)
 
-napoca_client_ret_code = subprocess.call(["node", "./connect_test.js", "3000"])
+napoca_client_ret_code = subprocess.call(["node", "./connect_test.js", "3001"])
 
 napoca_server.kill()
 
