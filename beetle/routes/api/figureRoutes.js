911eba39d350e1935b6ca5973850dd7b0f4f1ba7
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2019-04-24 23:26:00 +0300

    Implement endpoint for compiler on backend

--- beetle/routes/api/figureRoutes.js
+++ beetle/routes/api/figureRoutes.js
@@ -9,6 +9,10 @@ const verifyToken = require('../../helpers/verifyToken')
 const ValidatorFactory = require('../../helpers/validatorFactory')
 const validatorFactory = new ValidatorFactory()
 
+const napocaConfig = require('../../config/napocaconfig')
+const napoca_client = require('napoca-client')
+const napocaClient = new napoca_client.NapocaClient("localhost:" + napocaConfig.port)
+
 router.get('/', verifyToken, function (req, res) {
     figureController.findAll(req, res)
 })
@@ -27,6 +31,17 @@ router.post('/', verifyToken, validatorFactory.figureValidator(), function (req,
 
 router.delete('/:id', verifyToken, function (req, res) {
     figureController.delete(req, res)
+})
+
+router.post('/compile', function (req, res) {
+    codeToCompile = (req.body.code ? String(req.body.code) : "")
+    napocaClient.requestParse(codeToCompile, 
+        compiled => {
+            res.status(200).json({compiled: compiled})
+        },
+        err => {
+            res.status(400).json({errors: err})
+        })
 })
 
 module.exports = router
