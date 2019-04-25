f7633e907dd9c10070778885e9148947d0027fb1
Darius Marian <marian.darius98@gmail.com>
2019-04-25 22:35:23 +0300

    * Some refactoring of rest api client

--- panda/lib/services/rest_api_client.dart
+++ panda/lib/services/rest_api_client.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:angular/angular.dart' show Inject, Injectable, OpaqueToken;
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:panda/common/credentials.dart';
 import 'package:panda/common/error_or.dart';
@@ -10,9 +10,22 @@ import 'auth_service.dart';
 
 const apiAddress = OpaqueToken<String>('apiAddress');
 
+class RestApiResponse {
+  final int statusCode;
+  final dynamic body;
+
+  RestApiResponse(http.Response response)
+      : statusCode = response.statusCode,
+        body = JsonDecoder().convert(response.body);
+
+  RestApiResponse.networkError()
+      : statusCode = -1,
+        body = {'message': 'Network error.'};
+}
+
 @Injectable()
 class RestApiClient {
-  final Client _client;
+  final http.Client _client;
   final String _apiAddress;
   final AuthService _authService;
 
@@ -27,7 +40,7 @@ class RestApiClient {
     if (response.statusCode == 200) {
       _authService.setAuthenticated(AuthData(
         authToken: _authService.authToken,
-        username: JsonDecoder().convert(response.body)['username'],
+        username: response.body['username'],
       ));
     } else {
       _authService.setNotAuthenticated();
@@ -35,90 +48,89 @@ class RestApiClient {
   }
 
   Future<ErrorOr<void>> register(Credentials credentials) async {
-    Response response;
-    try {
-      response = await post('/auth/register', {
-        'username': credentials.username,
-        'password': credentials.password,
-      });
-    } catch (error) {
-      return ErrorOr.unsuccessful("Network error.");
-    }
+    final response = await post('/auth/register', {
+      'username': credentials.username,
+      'password': credentials.password,
+    });
     if (response.statusCode == 200) {
       return ErrorOr.successful(true);
     }
-    return ErrorOr.unsuccessful(
-        JsonDecoder().convert(response.body)['message']);
+    return ErrorOr.unsuccessful(response.body['message']);
   }
 
   Future<ErrorOr<AuthData>> login(Credentials credentials) async {
-    Response response;
-    try {
-      response = await post('/auth/login', {
-        'username': credentials.username,
-        'password': credentials.password,
-      });
-    } catch (error) {
-      return ErrorOr.unsuccessful("Network error.");
-    }
+    final response = await post('/auth/login', {
+      'username': credentials.username,
+      'password': credentials.password,
+    });
     if (response.statusCode != 200) {
-      return ErrorOr.unsuccessful(
-          JsonDecoder().convert(response.body)['message']);
+      return ErrorOr.unsuccessful(response.body['message']);
     }
     return ErrorOr.successful(AuthData(
-        authToken: JsonDecoder().convert(response.body)['token'],
-        username: credentials.username));
+        authToken: response.body['token'], username: credentials.username));
   }
 
   Future<Map<String, dynamic>> fetchFigure(int id) async {
-    return JsonDecoder().convert((await get('/figure/$id')).body);
+    return (await get('/figure/$id')).body;
   }
 
   Future<List<dynamic>> fetchFigures() async {
-    return JsonDecoder().convert((await get('/figure')).body);
+    return (await get('/figure')).body;
   }
 
   Future<int> createFigure(String name) async {
-    return JsonDecoder().convert((await post('/figure', {
-      "name": name,
-      "code": "",
-    }))
-        .body);
+    return (await post('/figure', {"name": name, "code": ""})).body;
   }
 
   Future<TikzCompilationResult> compile(String code) async {
     final response = await post('/figure/compile', {'code': code});
-    final body = JsonDecoder().convert(response.body);
     if (response.statusCode == 400) {
-      return TikzCompilationResult.unsuccessful(body['errors']);
+      return TikzCompilationResult.unsuccessful(response.body['errors']);
     }
-    return TikzCompilationResult.successful(body['compiled']);
+    return TikzCompilationResult.successful(response.body['compiled']);
   }
 
-  Future<Response> get(String url) async {
-    final response = await _client.get(_apiAddress + url, headers: _headers);
+  Future<RestApiResponse> get(String url) async {
+    http.Response response;
+    try {
+      response = await _client.get(_apiAddress + url, headers: _headers);
+    } catch (error) {
+      print(error);
+      return RestApiResponse.networkError();
+    }
     if (response.statusCode == 401) {
       _authService.setNotAuthenticated();
     }
-    return response;
+    return RestApiResponse(response);
   }
 
-  Future<Response> post(String url, Map<String, String> body) async {
-    final response =
-        await _client.post(_apiAddress + url, headers: _headers, body: body);
+  Future<RestApiResponse> post(String url, Map<String, String> body) async {
+    http.Response response;
+    try {
+      response =
+          await _client.post(_apiAddress + url, headers: _headers, body: body);
+    } catch (error) {
+      print(error);
+      return RestApiResponse.networkError();
+    }
     if (response.statusCode == 401) {
       _authService.setNotAuthenticated();
     }
-    return response;
+    return RestApiResponse(response);
   }
 
-  Future<Response> put(String url, Map<String, String> body) async {
-    final response =
-        await _client.put(_apiAddress + url, headers: _headers, body: body);
+  Future<RestApiResponse> put(String url, Map<String, String> body) async {
+    http.Response response;
+    try {
+      response =
+          await _client.put(_apiAddress + url, headers: _headers, body: body);
+    } catch (error) {
+      return RestApiResponse.networkError();
+    }
     if (response.statusCode == 401) {
       _authService.setNotAuthenticated();
     }
-    return response;
+    return RestApiResponse(response);
   }
 
   Map<String, String> get _headers {
